368972883ffe4abfa999750a7db064a6f1d8a949
Alex Wolf <alexrudiwolf@gmail.com>
2020-02-01 17:45:08 -0500

    end of module 06

--- Tasker/Pages/CreateTask.cshtml.cs
+++ Tasker/Pages/CreateTask.cshtml.cs
@@ -26,6 +26,11 @@ namespace Tasker.Pages
 
         public IActionResult OnPost()
         {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
             _context.Tasks.Add(NewTask);
             _context.SaveChanges();
             return RedirectToPage("Index");
