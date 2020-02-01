2e851e6f2a846ac2c0b052782989e71bffe306d6
Alex Wolf <alexrudiwolf@gmail.com>
2020-02-01 17:03:10 -0500

    end of module-05

--- Tasker/Pages/Index.cshtml.cs
+++ Tasker/Pages/Index.cshtml.cs
@@ -9,9 +9,18 @@ namespace Tasker.Pages
 {
     public class IndexModel : PageModel
     {
+        public IEnumerable<Task> Tasks { get; set; }
+
+        private ApplicationDbContext _context;
+
+        public IndexModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public void OnGet()
         {
-
+            Tasks = _context.Tasks.OrderBy(x => x.Priority);
         }
     }
 }
