85c523e4ec50e89fc8cc0c15c72defe17beef18c
Sunshine <sunshine.jacobson@ks.gov>
2022-07-26 15:19:31 -0500

    Completed module\

--- Tasker/Pages/CreateTask.cshtml.cs
+++ Tasker/Pages/CreateTask.cshtml.cs
@@ -26,16 +26,17 @@ namespace Tasker.Pages
     
         public IActionResult OnPost()
         {
-            if (ModelState.IsValid)
-            {
-                _context.Tasks.Add(NewTask);
-                _context.SaveChanges();
-                return RedirectToPage("Index");
-            }
-            else
-            {
+            if (!ModelState.IsValid)
+            { 
                 return Page();
             }
+                _context.Tasks.Add(NewTask);
+                _context.SaveChanges();
+                return RedirectToPage("Index");  
+        }
+            
+               
+            
         }
 
         
